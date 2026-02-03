e8ebc7b7def9a08559f80ace9e0cef16a7090df6
Kenneth <kbaraholeo@educacion.navarra.com>
2026-02-03 10:08:57 +0100

    Uno acabado

--- src/ex01_count_word.py
+++ src/ex01_count_word.py
@@ -31,9 +31,22 @@ def count_word_in_file(path: str | Path, word: str) -> int:
     Errores:
     - Si el fichero no existe, lanza FileNotFoundError.
     - Si word está vacía o solo espacios, lanza ValueError.
-
-    Ejemplo:
-    Fichero: "Hola hola mundo"
-    word="hola" -> 2
     """
+    punt=list(string.punctuation) #lista de puntuaciones básicas
+    punt.append("etc.") #añadimos "etc." a la lista de puntuaciones
+    cont=0
+    with open(path , "r", encoding="utf-8") as file:
+        if not Path(path).is_file():
+            raise FileNotFoundError(f"El fichero {path} no existe.")
+        if word.strip()=="":
+            raise ValueError('La palabra no puede estar vacía o contener solo espacios.')
+        contenido=file.read().lower() #leemos el contenido y lo pasamos a minúsculas
+        for p in punt:
+            contenido=contenido.replace(p," ") #reemplazamos la puntuación por espacios
+            palabras=contenido.split() #separamos el contenido en palabras
+        for p in palabras:
+            if p==word.lower(): #comparo cada palabra con la palabra buscada
+                cont+=1 #incremento el contador si hay coincidencia
+    return cont
+
     raise NotImplementedError("Implementa count_word_in_file(path, word)")
